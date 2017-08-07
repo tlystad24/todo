9a9ed5c1ac67a19cfbbeb1d88aedb6e9af5bd6f6
Tobias Lystad <tobias24@me.com>
2017-08-07 18:38:47 +0200

    Basic functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,24 @@
 // Check off todos when clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 	
+});
+
+// delete todo when x is clocked
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+	event.stopPropagation(); 
+});
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13){
+		// record text input
+		todoInput = $(this).val();
+		// clear input
+		$(this).val("");
+		// create new li and add input
+		$("ul").append("<li><span>X</span> " + todoInput + "</li>");
+	}
 });
